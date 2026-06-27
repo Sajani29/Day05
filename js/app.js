f0a9ed8ad01b704303393129af23b0d363518a7e
Sajani <22cis0309@ms.sab.ac.lk>
2026-06-27 15:31:23 +0530

    fundamentals

--- js/app.js
+++ js/app.js
@@ -10,87 +10,104 @@
 
 // alert("Your decesion is correct");
 
-let name = "saman";
-let age = 18;
-let isAfterAL = false;
-let customers = [];
-let customer = {};
-let salary = 7500.0;
+// let name = "saman";
+// let age = 18;
+// let isAfterAL = false;
+// let customers = [];
+// let customer = {};
+// let salary = 7500.0;
 
-console.log(name);
-console.log(age);
-console.log(typeof customers);
-console.log(typeof customer);
-console.log(customers);
-console.log(customer);
-console.log(salary);
+// console.log(name);
+// console.log(age);
+// console.log(typeof customers);
+// console.log(typeof customer);
+// console.log(customers);
+// console.log(customer);
+// console.log(salary);
 
-//arithmatic operators
-let x =10;
-let y = "10";
+// //arithmatic operators
+// let x =10;
+// let y = "10";
 
-console.log(x+y);
+// console.log(x+y);
 
-//comparasion operator
-let x =10;
-let y = "10";
+// //comparasion operator
+// let x =10;
+// let y = "10";
 
-console.log(x==y);  // true
-console.log(x===y);
+// console.log(x==y);  // true
+// console.log(x===y);
 
-//boolean operator 
-let x= true;
-let y = false;
+// //boolean operator 
+// let x= true;
+// let y = false;
 
-if (x && y) {
-    console.log("true");
-} else {
-    console.log("false");
+// if (x && y) {
+//     console.log("true");
+// } else {
+//     console.log("false");
     
-}
+// }
 
-//if else(according to the condition)
+// //if else(according to the condition)
 
-    if (true) {
-        console.log("true");
-    } else {
-        console.log("false");
-    }
+//     if (true) {
+//         console.log("true");
+//     } else {
+//         console.log("false");
+//     }
 
-// switch(case wise)
-switch (day) {
-    case 1:
-        console.log("monday");
-        break;
-    default:
-        console.log("invalid");
-        break;
-}
-//for loop
+// // switch(case wise)
+// switch (day) {
+//     case 1:
+//         console.log("monday");
+//         break;
+//     default:
+//         console.log("invalid");
+//         break;
+// }
+// //for loop
 
-for (let i = 0; i <10; i++) {
-    console.log(i);
+// for (let i = 0; i <10; i++) {
+//     console.log(i);
     
-}
+// }
 
-//while loop
+// //while loop
 
-while (true) {
-    console.log("hello");
+// while (true) {
+//     console.log("hello");
     
-}
+// }
 
-let i = 10
-while (i > 0) {
-    console.log(i);
-    i --;
+// let i = 10
+// while (i > 0) {
+//     console.log(i);
+//     i--;
     
-}
+// }
 
-//do while loop
-let i = 10;
-do {
-    console.log(i);
-    i --;
-} while (i > 0);
+// //do while loop
+// let i = 10;
+// do {
+//     console.log(i);
+//     i--;
+// } while (i > 0);
+
+
+//function
+
+// function add (x,y){
+//     return x + y;
+// }
+// let results = add(12,20);
+// console.log(results);
+
+//arrays
+
+// let numbers = [1,true,2,3, "sajani",4,5, 12.0];
+// for (let i = 0; i < numbers.length; i++) {
+//     console.log(numbers[i]);   
+// }
+
 
