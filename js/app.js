dcbd6bc1808ee0d2d91c5e41c3aff4f76ca02179
Sajani <22cis0309@ms.sab.ac.lk>
2026-06-27 15:09:55 +0530

    fundamentals

---
+++ js/app.js
@@ -0,0 +1,96 @@
+//console.log("Hello Java Script :) ");
+
+// let name = "Sajani";
+// let age = 23;
+
+// name = "nethmini";
+// console.log(name);
+// console.log(typeof name);
+// console.log(age);
+
+// alert("Your decesion is correct");
+
+let name = "saman";
+let age = 18;
+let isAfterAL = false;
+let customers = [];
+let customer = {};
+let salary = 7500.0;
+
+console.log(name);
+console.log(age);
+console.log(typeof customers);
+console.log(typeof customer);
+console.log(customers);
+console.log(customer);
+console.log(salary);
+
+//arithmatic operators
+let x =10;
+let y = "10";
+
+console.log(x+y);
+
+//comparasion operator
+let x =10;
+let y = "10";
+
+console.log(x==y);  // true
+console.log(x===y);
+
+//boolean operator 
+let x= true;
+let y = false;
+
+if (x && y) {
+    console.log("true");
+} else {
+    console.log("false");
+    
+}
+
+//if else(according to the condition)
+
+    if (true) {
+        console.log("true");
+    } else {
+        console.log("false");
+    }
+
+// switch(case wise)
+switch (day) {
+    case 1:
+        console.log("monday");
+        break;
+    default:
+        console.log("invalid");
+        break;
+}
+//for loop
+
+for (let i = 0; i <10; i++) {
+    console.log(i);
+    
+}
+
+//while loop
+
+while (true) {
+    console.log("hello");
+    
+}
+
+let i = 10
+while (i > 0) {
+    console.log(i);
+    i --;
+    
+}
+
+//do while loop
+let i = 10;
+do {
+    console.log(i);
+    i --;
+} while (i > 0);
+
